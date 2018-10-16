12aa779d25a10f353f4d74587e252bc8998d3bdf
James <JamesPritchardProgrammer@gmail.com>
2018-10-16 14:12:31 +1100

    Merge branch 'master' of https://github.com/ProgrammerJames/TPK-Learning-App
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/team/titans/tpk_learning_app/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.team.titans.tpk_learning_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+	private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		/* Populate Database */
+		DatabaseHelper db = new DatabaseHelper(this);
+		db.populateWords();
+
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable(){
+			@Override
+			public void run() {
+				/* Create an Intent that will start the Menu-Activity. */
+				Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+				SplashActivity.this.startActivity(mainIntent);
+				SplashActivity.this.finish();
+			}
+		}, SPLASH_DISPLAY_LENGTH);
+	}
+}
