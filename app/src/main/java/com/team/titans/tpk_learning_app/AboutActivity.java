41d28cf1c63d938309e5461bb55cab9a0d5a4b98
James <JamesPritchardProgrammer@gmail.com>
2018-10-16 14:06:20 +1100

    Merge branch 'master' of https://github.com/ProgrammerJames/TPK-Learning-App
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/vcs.xml

--- app/src/main/java/com/team/titans/tpk_learning_app/AboutActivity.java
+++ app/src/main/java/com/team/titans/tpk_learning_app/AboutActivity.java
@@ -1,12 +1,16 @@
 package com.team.titans.tpk_learning_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
--- app/src/main/java/com/team/titans/tpk_learning_app/ResultSplashActivity.java
+++ app/src/main/java/com/team/titans/tpk_learning_app/ResultSplashActivity.java
@@ -1,13 +1,32 @@
 package com.team.titans.tpk_learning_app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ResultSplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result_splash);
+
+        Intent myIntent = getIntent();
+        String correct = myIntent.getStringExtra("correct");
+
+        Toast.makeText(getApplicationContext(), "Answer: " + correct, Toast.LENGTH_LONG).show();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(ResultSplashActivity.this, QuestionActivity.class);
+                ResultSplashActivity.this.startActivity(mainIntent);
+                ResultSplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
